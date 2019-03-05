ee0deb2a58d78c83a6066fd1de2c323b0e046b55
Joseph Glaser <jpg84@drexel.edu>
2019-03-05 18:34:25 -0500

    Continuing Integration Work

--- sim_encounters.py
+++ sim_encounters.py
@@ -117,51 +117,125 @@ def run_collision(GravitatingBodies, end_time, delta_time, save_dir, save_id, **
 #           Defining Functions          #
 # ------------------------------------- #
 
+def CutOrAdvance(enc_bodies, primary_sysID, converter=None):
+    bodies = enc_bodies.copy()
+    systems = stellar_systems.get_planetary_systems_from_set(bodies, converter=converter, RelativePosition=False)
+    # As this function is pulling from Multiples, there should never be more than 2 "Root" Particles ...
+    if len(systems) > 2:
+        print "Error: Encounter has more roots than expected! Total Root Particles:", len(systems)
+        return None
+    # Assign the Primary System to #1 and Perturbing System to #2
+    sys_1 = systems[int(primary_sysID)]
+    secondary_sysID = [key for key in systems.keys() if key!=int(primary_sysID)][0]
+    sys_2 = systems[secondary_sysID]
 
+    mass_ratio = sys_2.mass.sum()/sys_1.mass.sum()
+    total_mass = sys_1.mass.sum() + sys_2.mass.sum()
+    rel_pos = sys_1.center_of_mass() - sys_2.center_of_mass()
+    rel_vel = sys_1.center_of_mass_velocity() - sys_2.center_of_mass_velocity()
+
+    kep = Kepler(unit_converter = converter, redirection = 'none')
+    kep.initialize_code()
+    kep.initialize_from_dyn(total_mass, rel_pos[0], rel_pos[1], rel_pos[2], rel_vel[0], rel_vel[1], rel_vel[2])
+
+    p = kep.get_periastron()
+    ignore_distance = mass_ratio**(1./3.) * 600 | units.AU
+    if p > ignore_distance:
+        print "Encounter Ignored due to Periastron of", p, "and an IgnoreDistance of", ignore_distance
+        return None
+    # Move the Particles to be Relative to their Respective Center of Mass
+    for particle in sys_1:
+        particle.position -= sys_1.center_of_mass()
+        particle.velocity -= sys_1.center_of_mass_velocity()
+    for particle in sys_2:
+        particle.position -= sys_2.center_of_mass()
+        particle.velocity -= sys_2.center_of_mass_velocity()
+
+    # Advance the Center of Masses to the Desired Distance in Reduced Mass Coordinates
+    kep.advance_to_radius(ignore_distance)
+    x, y, z = kep.get_separation_vector()
+    rel_pos_f = rel_pos.copy()
+    rel_pos_f[0], rel_pos_f[1], rel_pos_f[2] = x, y, z
+    vx, vy, vz = kep.get_velocity_vector()
+    rel_vel_f = rel_vel.copy()
+    rel_vel_f[0], rel_vel_f[1], rel_vel_f[2] = vx, vy, vz
+
+    # Transform to Absolute Coordinates from Kepler Reduced Mass Coordinates
+    cm_pos_1, cm_pos_2 = -sys_2.mass.sum() * rel_pos_f / total_mass, sys_1.mass.sum() * rel_pos_f / total_mass
+    cm_vel_1, cm_vel_2 = -sys_2.mass.sum() * rel_vel_f / total_mass, sys_1.mass.sum() * rel_vel_f / total_mass
+    # Move the Particles to the New Postions of their Respective Center of Mass
+    for particle in sys_1:
+        particle.position += cm_pos_1
+        particle.velocity += cm_vel_1
+    for particle in sys_2:
+        particle.position += cm_pos_2
+        particle.velocity += cm_vel_2
+    kep.stop()
+    return ParticlesSuperset([sys_1, sys_2])
 
 # ------------------------------------- #
 #         Main Production Script        #
 # ------------------------------------- #
-
 if __name__=="__main__":
 
-# ------------------------------------- #
-#      Setting up Required Variables    #
-# ------------------------------------- #
-cluster_name =
-max_number_of_rotations = 100
+    # ------------------------------------- #
+    #      Setting up Required Variables    #
+    # ------------------------------------- #
+    cluster_name =
+    base_planet_ID = 50000
+    max_number_of_rotations = 100
 
 
 
-# ------------------------------------- #
-#   Defining File/Directory Structure   #
-# ------------------------------------- #
-output_MainDirectory = os.getcwd()+"/Encounters"
-if not os.path.exists(output_MainDirectory): os.mkdir(output_MainDirectory)
+    # ------------------------------------- #
+    #   Defining File/Directory Structure   #
+    # ------------------------------------- #
+    output_MainDirectory = os.getcwd()+"/Encounters"
+    if not os.path.exists(output_MainDirectory): os.mkdir(output_MainDirectory)
 
 
-# Read in Encounter Directory
-encounter_file = os.getcwd()+cluster_name+"_encounters.pkl"
-encounter_db = pickle.load(encounter_file)
-encounter_file.close()
+    # Read in Encounter Directory
+    encounter_file = os.getcwd()+cluster_name+"_encounters.pkl"
+    encounter_db = pickle.load(encounter_file)
+    encounter_file.close()
 
-# Preform a Cut on the Encounter Database
+    # Perform a Cut on the Encounter Database
+    for star_ID in encounter_db.keys():
+        # Cut Out Stars Recorded with Only Initialization Pickups
+        if len(encounter_db[star_ID]) <= 1:
+            del encounter_db[star_ID]
+            continue
+        # Cut Out Stars with No Planets
+        for encounter in encounter_db[star_ID]:
+            # Refine "No Planet" Cut to Deal with Hierarchical Stellar Systems
+            # We are Looping Through Encounters to Deal with Rogue Jupiter Captures
+            if len([ID for ID in encounter.id if ID <= base_planet_ID]) == 0:
+                del encounter
+                continue
 
+    # Perform Cut & Advancement on Systems to Lower Integration Time
+    for star_ID in encounter_db.keys():
+        for enc_id, encounter in enumerate(encounter_db[star_ID]):
+            PeriastronCut = CutOrAdvance(encounter)
+            if PeriastronCut != None:
+                encounter_db[star_ID][enc_id] = PeriastronCut
+            elif PeriastronCut == None:
+                del encounter_db[star_ID][enc_id]
 
-# Begin Looping Through Stars (Each Star is a Pool Process)
-for star_id in encounter_db.keys():
-    output_KeyDirectory = output_MainDirectory+"/"+str(star_id)
-    if not os.path.exists(output_KeyDirectory): os.mkdir(output_KeyDirectory)
-    for encounter in encounter_db[star_id]:
-        encounter_id = 0
-        rotation_id= 0
-        output_EncDirectory = output_KeyDirectory+"/Enc-"+str(encounter_number)+"Rot-"+str(rotation_id)
-        if not os.path.exists(output_EncDirectory): os.mkdir(output_EncDirectory)
-        while rotation_id <= max_number_of_rotations:
-            # Add Other Planets
-            # Preform Random Rotation
-            # Store Initial Conditions
-            # Run Encounter
-            # Store Final Conditions
-            rotation_id += 1
-        encounter_id += 1
+    # Begin Looping Through Stars (Each Star is a Pool Process)
+    for star_id in encounter_db.keys():
+        output_KeyDirectory = output_MainDirectory+"/"+str(star_id)
+        if not os.path.exists(output_KeyDirectory): os.mkdir(output_KeyDirectory)
+        for encounter in encounter_db[star_id]:
+            encounter_id = 0
+            rotation_id= 0
+            output_EncDirectory = output_KeyDirectory+"/Enc-"+str(encounter_number)+"Rot-"+str(rotation_id)
+            if not os.path.exists(output_EncDirectory): os.mkdir(output_EncDirectory)
+            while rotation_id <= max_number_of_rotations:
+                # Add Other Planets
+                # Preform Random Rotation
+                # Store Initial Conditions
+                # Run Encounter
+                # Store Final Conditions
+                rotation_id += 1
+            encounter_id += 1
