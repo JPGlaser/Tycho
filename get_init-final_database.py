d2b13a4cfee6ba6ef211a9dc0ed6cffbd237a1b2
Joseph Glaser <jpg84@drexel.edu>
2019-08-03 08:47:34 -0400

    Updates to main script.

--- get_init-final_database.py
+++ get_init-final_database.py
@@ -23,12 +23,21 @@ from tycho import util, create, read, write, stellar_systems
 
 set_printing_strategy("custom", preferred_units = [units.MSun, units.AU, units.day], precision = 6, prefix = "", separator = "[", suffix = "]")
 
-def get_first_and_last_states(bodies, end_time=(10 | units.Myr)):
+def get_first_and_last_states(bodies, end_time=(10 | units.Myr), kepler_worker=None):
+    if kepler_worker == None:
+        if converter == None:
+            converter = nbody_system.nbody_to_si(1 | units.MSun, 100 |units.AU)
+        kep_p = Kepler(unit_converter = converter, redirection = 'none')
+        kep_p.initialize_code()
+    else:
+        kep_p = kepler_worker
     first = bodies.get_state_at_timestamp(0 | units.yr)
     last = bodies.get_state_at_timestamp(end_time)
     result = [first, last]
     for state in result:
-        stellar_systems.update_host_star(state)
+        stellar_systems.update_host_star(state, kepler_worker=kep_p)
+    if kepler_worker ==None:
+        kep_p.stop()
     return (first, last)
 
 if __name__=="__main__":
@@ -41,14 +50,21 @@ if __name__=="__main__":
                       help="Enter the full directory of the Root Folder.")
     parser.add_option("-S", "--serial", dest="doSerial", action="store_true",
                       help="Run the program in serial?.")
+    parser.add_option("-r", "--sample", dest="sample_rate", default=10, type="int",
+                      help="Sampling Rate for the clusters.")
     (options, args) = parser.parse_args()
     if options.rootDir != None:
         rootDir = options.rootDir
     else:
         rootDir = '/home/draco/jglaser/Public/Tycho_Runs/MarkG/'
     doSerial = options.doSerial
+    sample_rate = options.sample_rate
 
-    paths_of_hdf5_files = glob.glob(rootDir+'*/Encounters/*/Enc-*Rot-*.hdf5')
+    # Bring Root Directory Path Inline with os.cwd()
+    if rootDir.endswith("/"):
+        rootDir = rootDir[:-1]
+
+    paths_of_hdf5_files = glob.glob(rootDir+'/*/Encounters/*/Enc-*Rot-*.hdf5')
     cluster_names = [path.split("/")[-4] for path in paths_of_hdf5_files]
     primary_sysIDs = [path.split("/")[-2] for path in paths_of_hdf5_files]
     enc_IDs = [path.split("/")[-1].split("-")[1].split('Rot')[0] for path in paths_of_hdf5_files]
@@ -60,17 +76,22 @@ if __name__=="__main__":
             counter_finished += 1
     print "Number of Encounters Fully Simulated:", counter_finished
     print "Number of Initial States Simulated:", len(rot_IDs)
-    print "Total Number of Initial States to Simulate:", int(len(glob.glob(rootDir+'*/Encounters/*/')))*100
+    print "Total Number of Initial States to Simulate:", int(len(glob.glob(rootDir+'/*/Encounters/*/')))*101
 
+    converter = nbody_system.nbody_to_si(1 | units.MSun, 100 |units.AU)
+    kep = Kepler(unit_converter = converter, redirection = 'none')
+    kep.initialize_code()
 
     total_flDB = defaultdict(list)
-    for i, path in enumerate(paths_of_hdf5_files[::10]):
+    for i, path in enumerate(paths_of_hdf5_files[::sample_rate]):
         system = read_set_from_file(path, 'hdf5',version='2.0', copy_history=True, close_file=True)
-        f_and_l = get_first_and_last_states(system)
+        f_and_l = get_first_and_last_states(system, kepler_worker=kep)
         total_flDB[cluster_names[i]].append(f_and_l)
         if i%10==0:
-            print "!!!!!! Percent Completed:", i*1.0/len(paths_of_hdf5_files[::10])*100
+            print "!!!!!! Percent Completed:", i*1.0/len(paths_of_hdf5_files[::sample_rate])*100
 
     flDB_file = open(rootDir+"TotalI-F_DB.pkl", "w")
     pickle.dump(total_flDB, flDB_file)
     flDB_file.close()
+
+    kep.stop()
