aa7982a2ccd06d80dccd3ff9c32828f1ddc7467b
Joseph Glaser <jpg84@drexel.edu>
2020-07-18 12:44:06 -0400

    Update to sim_cluster using new function update_leaves_pos_vel.

--- sim_cluster.py
+++ sim_cluster.py
@@ -534,13 +534,16 @@ if __name__=="__main__":
         # Evolve the Gravitational Codes ( via Bridge Code)
         bridge_code.evolve_model(t_current)
 
+        # Update the Leaves of the Multiples Code Particles
+        multiples_code.update_leaves_pos_vel()
+
         # Sync the Gravitational Codes w/ the "Gravitating_Bodies" Superset
         channel_from_multi_to_gravitating.copy_attributes(['x', 'y', 'z', 'vx', 'vy', 'vz'])
 
         # (On a Copy) Recursively Expand All Top-Level Parent Particles & Update Subsets
         # Note: This Updates the Children's Positions Relative to their Top-Level Parent's Position
-        subset_sync = ChildUpdater()
-        subset_sync.update_children_bodies(multiples_code, Individual_Stars, Planets)
+        #subset_sync = ChildUpdater()
+        #subset_sync.update_children_bodies(multiples_code, Individual_Stars, Planets)
 
         # Evolve the Stellar Codes (via SEV Code with Channels)
         # TODO: Ensure Binaries are Evolved Correctly (See Section 3.2.8)
